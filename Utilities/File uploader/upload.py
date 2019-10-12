764c0da46ca3ff8924177f951ce1b2d013006b28
James Chen <jchen3652@gmail.com>
2019-10-12 09:22:12 -0400

    Add utilities

---
+++ Utilities/File uploader/upload.py
@@ -0,0 +1,41 @@
+def import_product_sets(project_id, location, gcs_uri):
+    """Import images of different products in the product set.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        gcs_uri: Google Cloud Storage URI.
+            Target files must be in Product Search CSV format.
+    """
+    client = vision.ProductSearchClient()
+
+    # A resource that represents Google Cloud Platform location.
+    location_path = client.location_path(
+        project=project_id, location=location)
+
+    # Set the input configuration along with Google Cloud Storage URI
+    gcs_source = vision.types.ImportProductSetsGcsSource(
+        csv_file_uri=gcs_uri)
+    input_config = vision.types.ImportProductSetsInputConfig(
+        gcs_source=gcs_source)
+
+    # Import the product sets from the input URI.
+    response = client.import_product_sets(
+        parent=location_path, input_config=input_config)
+
+    print('Processing operation name: {}'.format(response.operation.name))
+    # synchronous check of operation status
+    result = response.result()
+    print('Processing done.')
+
+    for i, status in enumerate(result.statuses):
+        print('Status of processing line {} of the csv: {}'.format(
+            i, status))
+        # Check the status of reference image
+        # `0` is the code for OK in google.rpc.Code.
+        if status.code == 0:
+            reference_image = result.reference_images[i]
+            print(reference_image)
+        else:
+            print('Status code not OK: {}'.format(status.message))
+
+import_product_sets("allergy-compass", "usa-east1", "gs://allergy-compass.appspot.com/product-set.csv")
---
+++ Utilities/FoodFinder.py
@@ -0,0 +1,16 @@
+# Standalone assistant to search for products
+import openfoodfacts
+import json
+
+searchTerm = "lay's baked"
+
+raw = openfoodfacts.products.search(searchTerm) # produces a json
+
+for entry in raw["products"]:
+  #  print(entry)
+    print(entry["product_name"])
+    print(entry["code"])
+    print(entry["allergens_hierarchy"])
+    print(entry["url"])
+    print("\n")
+    
