70889022c04d1a8df4a18a2a101352afbcb389c0
James Chen <jchen3652@gmail.com>
2019-10-12 06:00:09 -0400

    Move vision function to same file

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 import base64
-import visionAPI
+from google.cloud import vision
 from .models import Greeting
 
 # Create your views here.
@@ -40,3 +40,38 @@ def images(request):
         return HttpResponse(imageURLToFoodID(url))
     elif request.method == 'GET':
         return render(request, "imageView.html")
+
+def imageURLToFoodID(url):
+    """Search similar products to image.
+    Args:
+        url
+    """
+    # product_search_client is needed only for its helper methods.
+    product_search_client = vision.ProductSearchClient()
+    image_annotator_client = vision.ImageAnnotatorClient()
+
+    #TODO: this may be incorrect for network stuff
+    with open(url, 'rb') as image_file:
+        content = image_file.read()
+
+    # Create annotate image request along with product search feature.
+    image = vision.types.Image(content=content)
+
+    # product search specific parameters
+    product_set_path = product_search_client.product_set_path(
+        project=project_id, location=location,
+        product_set=product_set_id)
+    product_search_params = vision.types.ProductSearchParams(
+        product_set=product_set_path,
+        product_categories=[product_category],
+        filter=filter)
+    image_context = vision.types.ImageContext(
+        product_search_params=product_search_params)
+
+    # Search products similar to the image.
+    response = image_annotator_client.product_search(
+        image, image_context=image_context)
+
+    results = response.product_search_results.results
+
+    return results[0].product.name
