b613c5a4b5d43112016c6c3ba18f9ab8f9823123
James Chen <jchen3652@gmail.com>
2019-10-11 21:50:13 -0400

    Responds at /test

--- hello/views.py
+++ hello/views.py
@@ -17,3 +17,6 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+def test(request):
+    return HttpResponse("Allahu akbar")
