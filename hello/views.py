472b3541ec77057a380df204b4d79446dd6dcf22
James Chen <jchen3652@gmail.com>
2019-10-13 05:23:21 -0400

    The Great Purge

--- hello/views.py
+++ hello/views.py
@@ -32,18 +32,6 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
-
-@csrf_exempt
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
-
-
 @csrf_exempt
 def preferenceUpdate(request):
     print("Request body: ", request.body)
@@ -218,63 +206,6 @@ def images(request):
 
     elif request.method == 'GET':
         return render(request, "imageView.html")
-
-#
-#
-# def get_similar_products_file(
-#         project_id, location, product_set_id, product_category,
-#         file_path, filter):
-#     """Search similar products to image.
-#     Args:
-#         project_id: Id of the project.
-#         location: A compute region name.
-#         product_set_id: Id of the product set.
-#         product_category: Category of the product.
-#         file_path: Local file path of the image to be searched.
-#         filter: Condition to be applied on the labels.
-#         Example for filter: (color = red OR color = blue) AND style = kids
-#         It will search on all products with the following labels:
-#         color:red AND style:kids
-#         color:blue AND style:kids
-#     """
-#     # product_search_client is needed only for its helper methods.
-#     product_search_client = vision.ProductSearchClient()
-#     image_annotator_client = vision.ImageAnnotatorClient()
-#
-#     # Read the image as a stream of bytes.
-#     with open(file_path, 'rb') as image_file:
-#         content = image_file.read()
-#
-#     # Create annotate image request along with product search feature.
-#     image = vision.types.Image(content=content)
-#
-#     # product search specific parameters
-#     product_set_path = product_search_client.product_set_path(
-#         project=project_id, location=location,
-#         product_set=product_set_id)
-#     product_search_params = vision.types.ProductSearchParams(
-#         product_set=product_set_path,
-#         product_categories=[product_category],
-#         filter=filter)
-#     image_context = vision.types.ImageContext(
-#         product_search_params=product_search_params)
-#
-#     # Search products similar to the image.
-#     response = image_annotator_client.product_search(
-#         image, image_context=image_context)
-#
-#     # print(response)
-#
-#     index_time = response.product_search_results.index_time
-#     print('Product set index time:')
-#     print('  seconds: {}'.format(index_time.seconds))
-#     print('  nanos: {}\n'.format(index_time.nanos))
-#
-#     results = response.product_search_results.
-#
-#     return results[0].product.name
-#
-
 
 def get_similar_products_uri(
         project_id, location, product_set_id, product_category,
